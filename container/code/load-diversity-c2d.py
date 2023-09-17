3838cfa01f12bb4fb2033d0b7891c96b5b410ddc
Niels Klomp <nklomp@sphereon.com>
2023-09-17 21:16:06 +0200

    fix: Input file detection, memory usage

--- container/code/load-diversity-c2d.py
+++ container/code/load-diversity-c2d.py
@@ -1,49 +1,93 @@
-
 import pandas
+import gc
 import os
 import numpy as np
 import math
+import magic
+import sys
 import matplotlib.pyplot as pyplot
 import scipy.stats as stats
 
+INPUT_DIR = "/data/inputs"  # location of input data (regular or zipped CSV) with load user load profiles
+OUTPUT_DIR = "/data/outputs"  # location of output data (PDF)
+LOG = "/data/logs/load-diversity-c2d.log"
+SAMPLESIZE = 50 #100 # Number of independent samples to use to estimate averages
+NUMBOOTSTRAP = 1000 #2000  # Number of bootstrap resampling steps
+STEPFACTOR = 6 #3  # Aggregation level step size (multiplicative, i.e. 3 gives 1,3,9,...)
 
-INPUT_DIR = "/data/inputs"                   # location of input data (regular or zipped CSV) with load user load profiles
-OUTPUT_DIR = "/data/outputs"                # location of output data (PDF)
-SAMPLESIZE = 100                        # Number of independent samples to use to estimate averages
-NUMBOOTSTRAP = 2000                     # Number of bootstrap resampling steps
-STEPFACTOR = 2                          # Aggregation level step size (multiplicative, i.e. 3 gives 1,3,9,...)
+
+def log(msg, exit_code=-1):
+    print(msg)
+    # print(msg, file=open(LOG, 'a'))
+
+    if exit_code >= 0:
+        sys.exit(exit_code)
+
+def memory_stats(msg):
+    log(msg)
+    os.system("cat /proc/meminfo | grep Mem")
+
+memory_stats("Start of script")
 
 ## Load data
+input_type = None
+input_file = None
 
-input_found = False
+
 for basedir, subdir, files in os.walk(INPUT_DIR):
-    if input_found:
+    if input_type is not None:
         break
     for file in files:
-        DATA = os.path.join(basedir, file)
-        if file.lower().endswith(".csv") or file.lower().endswith(".csv.zip"):
-            print(f"Using data file {DATA}\n")
-            input_found = True
+        input_file = os.path.join(basedir, file)
+        mime_type = magic.from_file(input_file)
+        if "csv" in mime_type.lower():
+            log(f"Using csv data file {input_file}")
+            input_type = "csv"
+            break
+        elif "zip" in mime_type.lower():
+            log(f"Using zip data file {input_file}")
+            input_type = "zip"
             break
         else:
-            print(f"Skipping {DATA} (not a .CSV or .CSV.ZIP file)")
-if not input_found:
-    print(f"Error: no .CSV or .CSV.ZIP file found in {INPUT_DIR}")
+            log(f"Skipping {input_file} (not a .CSV or .CSV.ZIP file. Mime type: ${mime_type.lower()})")
 
-dataframe = pandas.read_csv(DATA, index_col=0, parse_dates=True)
+
+dataframe = None
+# dtypes = {} #{0: 'string'}
+#
+# for i in range(1, 4444):
+#      dtypes[i] = 'float16'
+
+if input_type == "csv":
+    log(f"Start reading {input_file} csv file...")
+    dataframe = pandas.read_csv(input_file, index_col=0, parse_dates=True, dtype='float32')
+    log(f"Finished reading {input_file} csv file")
+elif input_type == "zip":
+    # Note that this branch of the expression is currently not being used because of an issue on the C2D when reading zips directly
+    log(f"Start reading {input_file} csv file from zip...")
+    dataframe = pandas.read_csv(input_file, compression=input_type, index_col=0, parse_dates=True, dtype='float32')
+    log(f"Finished reading {input_file} csv file from zip")
+else:
+    log(f"Error: no .CSV or .CSV.ZIP file found in {INPUT_DIR}", 1)
+
+memory_stats("after reading csv")
+# log(f"Data types {dataframe.dtypes}")
+gc.collect()
+memory_stats("after reading csv and gc")
 
 ## Sampling and diversity factor calculation
-
 # determine the number of aggregation levels according to the size of the dataset and the STEPFACTOR
-aggregation_steps = 1 + math.floor(math.log(len(dataframe.columns))/math.log(STEPFACTOR))
+aggregation_steps = 1 + math.floor(math.log(len(dataframe.columns)) / math.log(STEPFACTOR))
 # calculate the aggregation levels, starting at 1
-aggregation_levels = [STEPFACTOR**i for i in range(aggregation_steps)]
+aggregation_levels = [STEPFACTOR ** i for i in range(aggregation_steps)]
 
-print(f"Sampling diversity factors for {len(aggregation_levels)} aggregation levels:")
+log(f"Sampling diversity factors for {len(aggregation_levels)} aggregation levels:")
 # initialise result array
 diversity_factor_array = np.zeros((len(aggregation_levels), SAMPLESIZE))
 # for every aggregation level and sample count
 for agg_step, households in enumerate(aggregation_levels):
+    gc.collect()
+    memory_stats(f"agg step: {agg_step}")
     for i in range(SAMPLESIZE):
         # sample 'households' with replacement
         sample = dataframe.sample(n=households, axis='columns', replace=True)
@@ -57,29 +101,45 @@ for agg_step, households in enumerate(aggregation_levels):
             diversity_factor = 1.0
         # store value in array
         diversity_factor_array[agg_step, i] = diversity_factor
-print(f"Sampling complete.\n")
+log(f"Sampling complete.")
 
+memory_stats("after sampling")
+log(f"GC count after sampling: {gc.get_count()}")
+gc.collect()
+log(f"GC count after dataframe sampling GC calls: {gc.get_count()}")
+memory_stats("after sampling and GC")
+dataframe = None
 ## Postprocessing
 
-print(f"Postprocessing results:")
+log(f"GC count after dataframe null: {gc.get_count()}")
+gc.collect()
+log(f"GC count after datatframe null GC calls: {gc.get_count()}")
+memory_stats("After dataframe null")
+
+
+log(f"Postprocessing results:")
 # calculate sample average for each aggregation level
 average_diversity_factor = diversity_factor_array.mean(axis=1)
 
 # identify sample sets with non-identical values (usually all other than aggregation level = 1)
-bootstrap_filter = (np.equal(diversity_factor_array[:,0:1], diversity_factor_array).all(axis=1) == False)
+bootstrap_filter = (np.equal(diversity_factor_array[:, 0:1], diversity_factor_array).all(axis=1) == False)
 # apply BCa bootstrap analysis
-bootstrap_stats = stats.bootstrap((diversity_factor_array[bootstrap_filter],), np.mean, n_resamples=NUMBOOTSTRAP, axis=1, vectorized=True)
+bootstrap_stats = stats.bootstrap((diversity_factor_array[bootstrap_filter],), np.mean, n_resamples=NUMBOOTSTRAP,
+                                  axis=1, vectorized=True)
 
 # extract bootstrap standard errors (and use zero where all samples are identical)
 standard_error = np.zeros(aggregation_steps)
 standard_error[bootstrap_filter] = bootstrap_stats.standard_error
 
 # extract absolute up / down errors
-diversity_factor_errors = np.zeros((2,aggregation_steps))
-diversity_factor_errors[0, bootstrap_filter] = average_diversity_factor[bootstrap_filter] - bootstrap_stats.confidence_interval.low
-diversity_factor_errors[1, bootstrap_filter] = bootstrap_stats.confidence_interval.high - average_diversity_factor[bootstrap_filter]
+diversity_factor_errors = np.zeros((2, aggregation_steps))
+diversity_factor_errors[0, bootstrap_filter] = average_diversity_factor[
+                                                   bootstrap_filter] - bootstrap_stats.confidence_interval.low
+diversity_factor_errors[1, bootstrap_filter] = bootstrap_stats.confidence_interval.high - average_diversity_factor[
+    bootstrap_filter]
 
-print(f"Postprocessing complete.\n")
+log(f"Postprocessing complete.")
+
 
 ## Output
 
@@ -107,7 +167,7 @@ def round_se(x, se):
         digits = digits_x - digits_se + 2
         # construct return string
         return f"{np.format_float_positional(x, precision=digits, fractional=False, trim='k')} " + \
-                f"({np.format_float_positional(se, precision=2, fractional=False, trim='k')})"
+            f"({np.format_float_positional(se, precision=2, fractional=False, trim='k')})"
     elif (se == 0):
         # Precision is infinite, so report using standard precision
         return np.format_float_positional(x, trim='k') + " (0)"
@@ -117,9 +177,9 @@ def round_se(x, se):
 
 
 # print tabular output
-print("    #      diversity factor (standard error)")
+log("    #      diversity factor (standard error)")
 for idx, households in enumerate(aggregation_levels):
-    print(f"{households:5}      {round_se(average_diversity_factor[idx], standard_error[idx])}")
+    log(f"{households:5}      {round_se(average_diversity_factor[idx], standard_error[idx])}")
 
 # plot results
 
@@ -132,3 +192,4 @@ pyplot.title('diversity factors with 95% confidence intervals')
 # save a PDF copy
 pyplot.savefig(os.path.join(OUTPUT_DIR, 'diversity_factor.pdf'))
 
+log("Done", 0)
